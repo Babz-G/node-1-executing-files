e264c60c60678711c0be7f0676dc5491e8598e78
Babz-G <barbaramgaynor@gmail.com>
2026-02-25 16:40:19 -0500

    initial commit

--- src/reverseString.js
+++ src/reverseString.js
@@ -1,6 +1,6 @@
 // 🧠 Your Task:
-// 1. Write JavaScript code below that reverses the `inputString` using a for loop.
-// 2. Store the reversed string in the variable called `reversedString`.
+// ✅ 1. Write JavaScript code below that reverses the `inputString` using a for loop.
+// ✅ 2. Store the reversed string in the variable called `reversedString`.
 // 3. Use `console.log()` to print the value of `reversedString`.
 // 4. Run this file in your terminal using: `node reverseString.js`
 
@@ -10,9 +10,26 @@
 
 // -------------------------------------
 
+// ✨✨ EXAMPLE FROM STACKOVERFLOW ✨✨
+// Source - https://stackoverflow.com/q/48091849
+// Posted by horcrux88, modified by community. See post 'Timeline' for change history
+
+// function reverse(str){
+//     var reversedString = '';
+//     for (var i = str.length - 1; i >= 0; i--){
+//        reversedString = reversedString + str[i];
+// }
+// return reversedString;
+// }
+
+// alert(reverse('hello'));
+
 const inputString = "Javascript Rocks!";
 let reversedString = "";
 
 // Your code here
+for (var i = inputString.length - 1; i >= 0; i--) {
+  reversedString = reversedString + inputString[i];
+}
 
 console.log(reversedString);
